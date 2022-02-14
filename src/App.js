2a9a90be4ae97e0c21906a95909ec4d4064019fd
Riley <riley.kamstra@gmail.com>
2022-02-14 09:58:15 +0200

    Start Quizz button fuctionality working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './styles.css'
 import IntroScreen from './components/IntroScreen'
 
 function App() {
+    // Create state to track if the 'Start Quiz button was clicked'
+    const [startQuiz, setStartQuiz] = useState(false)
+
+    // Handle the click event from the 'Start Quiz' button
+    function handleClick() {
+        // If 'Start Quiz' button click set startQuiz to true
+        setStartQuiz(prevStartQuiz => !prevStartQuiz)
+    }
+
     return (
-        <IntroScreen/>
+        !startQuiz && <IntroScreen startQuiz={handleClick}/>
     )
 }
 
--- src/components/IntroScreen.js
+++ src/components/IntroScreen.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-function IntroScreen() {
+
+/* 
+ * Accept props for the 'Start Quiz button
+ * If the button is clicked it must set the StartQuiz bool to true
+ */
+
+function IntroScreen(props) {
     return (
         <div className='intro-container'>
             <h1>Quizzical</h1>
             <h4>Test your Trivia</h4>
-            <button className='start-quiz-button'>Start Quiz</button>
+            <button className='start-quiz-button' onClick={props.startQuiz}>Start Quiz</button>
         </div>
     )
 }
